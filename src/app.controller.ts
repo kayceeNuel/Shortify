17120fa32050e91427bb4f24f3d061622ea17a83
Kaycee Nuel <heykayceee@gmail.com>
2024-01-15 23:46:32 +0100

    updating endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Query} from '@nestjs/common';
 import { AppService } from './app.service';
+import { url } from 'inspector';
+import { Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,9 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Post('shorten') 
+  shorten(@Query('url') url:string): Observable<string>{
+     return this.appService.shorten(url)
   }
 }
