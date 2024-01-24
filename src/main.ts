29a52a934395016929024cd47972a5735510c3aa
Kaycee Nuel <heykayceee@gmail.com>
2024-01-24 22:06:40 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
-
-import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-export async function bootstrap() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger;
-  const port = process.env.PORT;
-
-  app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(port);
-  
-  logger.log(`server listening on port${port}`);
+  await app.listen(3000);
 }
 bootstrap();
