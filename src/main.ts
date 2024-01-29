bf130154fcfaf093d8eb9c599761779c6011e3fb
Kaycee Nuel <heykayceee@gmail.com>
2024-01-29 21:06:03 +0100

    updating rootfile

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe} from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes( new ValidationPipe());
+  app.useGlobalPipes( new ValidationPipe({transform: true, whitelist: true}));
   await app.listen(3000);
 }
 bootstrap();
