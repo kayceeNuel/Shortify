407b6160115ddabb9a94679661a941d658c2146a
Kaycee Nuel <heykayceee@gmail.com>
2024-02-03 23:19:26 +0100

    updating modules

--- src/url-shortener/url.module.ts
+++ src/url-shortener/url.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { UrlController } from "./url.contoller";
 import { UrlService } from "./url.service"
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UrlEntity } from "../entity/url.entity";
 
 @Module({
+    imports: [TypeOrmModule.forFeature([UrlEntity])],
     controllers: [UrlController],
     providers: [UrlService], 
 })
