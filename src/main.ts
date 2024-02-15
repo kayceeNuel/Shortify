385bdf111dfac9d32771d0ec665253a945d17d82
Kaycee Nuel <heykayceee@gmail.com>
2024-02-15 23:29:11 +0100

    updating main file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { ValidationPipe} from '@nestjs/common';
+import { ValidationPipe, Logger} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const logger = new Logger()
+
   app.useGlobalPipes( new ValidationPipe({
     transform: true, 
     whitelist: true
@@ -11,6 +14,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`)
+
+  logger.log (`Application is running on: http://localhost:${port}`)
 }
 bootstrap(); 
