5c2ca8cc613778ac7b48f9c6ac39468b23ac9fff
Kaycee Nuel <heykayceee@gmail.com>
2024-02-16 22:48:08 +0100

    initial changes

--- src/entity/url.entity.ts
+++ src/entity/url.entity.ts
@@ -1,7 +1,7 @@
 import { Column,Entity, PrimaryGeneratedColumn } from "typeorm";
 
     Entity()
-    export class UrlEntity {
+    export class Url {
     @PrimaryGeneratedColumn() 
     id: number;
 
