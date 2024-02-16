434a92bb9727752483445c763a49327b485708cf
Kaycee Nuel <heykayceee@gmail.com>
2024-02-16 22:55:11 +0100

    updating

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UrlModule }  from 'src/url-shortener/url.module';
 
 @Module({
   imports: [ UrlModule, ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
