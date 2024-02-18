96f4762fbd6c99a0c8b4c7012dc4a9591d50fcda
Kaycee Nuel <heykayceee@gmail.com>
2024-02-18 18:43:02 +0100

    working on TypeORM Module

---
+++ src/url-shortener/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from "@nestjs/common";
+import { UrlModule } from "./url.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+
+@Module ({ 
+  imports: [
+    ConfigModule.forRoot( { isGlobal: true}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService ) => ({
+        type: "postgres",
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'), 
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'), 
+        entities: [],
+        synchronize: true, 
+      
+      }),
+      inject: [ConfigService],
+    }),
+    UrlModule,
+  ], 
+  controllers: [],
+  providers: [], 
+})
+
+ export class AppModule {}
