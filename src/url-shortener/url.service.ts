d3958593e4b266f8a6014f2374f8408edabe7323
Kaycee Nuel <heykayceee@gmail.com>
2024-02-26 22:40:21 +0100

    update

--- src/url-shortener/url.service.ts
+++ src/url-shortener/url.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { Url } from '../entity/url.entity';
 import { InjectRepository } from "@nestjs/typeorm";
 import { CreateUrlDto } from "./dto/create-url.dto";
 import { nanoid } from "nanoid";
+
 
 
 
@@ -26,17 +27,19 @@ export class UrlService {
             return this.urlRepository.save(url);
         }
 
+
          //get All Url links
          async fetchAllUrl(): Promise<Url[]> {
             return this.urlRepository.find(); 
         }
 
+
         async redirectUrl  (shortUrl: string): Promise<string> {
-            const url = await this.urlRepository.findOneBy({ shortUrl}) 
-        
-            if (!url) {
-                throw new Error(' Url not found ')
-            }
-            return url.originalUrl
-        } 
+           const url = await this.urlRepository.findOneBy({shortUrl}) 
+           if(url) {
+            return url.originalUrl;
+           } else {
+            throw new NotFoundException('Resource Not Found');
+           }
+        }
     }
